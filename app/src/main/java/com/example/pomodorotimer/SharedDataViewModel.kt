ad1c4830a268e5ad2819db437edd178d95a56d9d
prchen030 <prchen030@gmail.com>
2025-09-10 16:16:25 +0200

    update

---
+++ app/src/main/java/com/example/pomodorotimer/SharedDataViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.pomodorotimer
+
+import android.content.Context
+import androidx.core.content.edit
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class SharedDataViewModel @Inject constructor(
+    @param:ApplicationContext private val context: Context
+) : ViewModel(){
+
+    private val prefs = context.getSharedPreferences("my_prefs",Context.MODE_PRIVATE)
+    private val _pomodoroTime =
+        MutableStateFlow(prefs.getInt(PrefKeys.KEY_POMODORO_TIME, R.integer.default_duration))
+    private val _shortBreakTime =
+        MutableStateFlow(prefs.getInt(PrefKeys.KEY_SHORT_BREAK_TIME, R.integer.default_short_break))
+    private val _longBreakTime =
+        MutableStateFlow(prefs.getInt(PrefKeys.KEY_LONG_BREAK_TIME, R.integer.default_short_break))
+    val pomodoroTime: StateFlow<Int> = _pomodoroTime
+    val shortBreakTime: StateFlow<Int> = _shortBreakTime
+    val longBreakTime: StateFlow<Int> = _longBreakTime
+
+    private val _ifNotification =
+        MutableStateFlow(prefs.getBoolean(PrefKeys.KEY_IF_NOTIFICATION, false))
+    private val _ifSound =
+        MutableStateFlow(prefs.getBoolean(PrefKeys.KEY_IF_SOUND, false))
+    val ifNotification: StateFlow<Boolean> = _ifNotification
+    val ifSound: StateFlow<Boolean> = _ifSound
+
+    fun updateIntValue(key: String, value: Int){
+        viewModelScope.launch {
+            prefs.edit { putInt(key, value)}
+            when(key){
+                PrefKeys.KEY_POMODORO_TIME -> _pomodoroTime.value = value
+                PrefKeys.KEY_SHORT_BREAK_TIME -> _shortBreakTime.value = value
+                PrefKeys.KEY_LONG_BREAK_TIME -> _longBreakTime.value = value
+            }
+        }
+    }
+
+    fun updateBooleanValue(key: String, value: Boolean){
+        viewModelScope.launch {
+            prefs.edit {
+                putBoolean(key, value)
+            }
+            when(key){
+                PrefKeys.KEY_IF_NOTIFICATION -> _ifNotification.value = value
+                PrefKeys.KEY_IF_SOUND -> _ifSound.value = value
+            }
+        }
+    }
+
+}
+
+
